0251d6bc7db60fc076dbc724201a4413f9d2c471
rhoda-lee <rhodalee.dev@gmail.com>
2025-08-09 07:57:11 +0000

    Add Program.cs to demonstrate all tasks: inheritance, abstract classes and interfaces

---
+++ Assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Inheritance & Method Overriding ===");
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        genericAnimal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+
+        Console.WriteLine("\n=== Abstract Classes ===");
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        Console.WriteLine($"Circle Area: {circle.GetArea()}");
+        Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+
+        Console.WriteLine("\n=== Interfaces ===");
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
